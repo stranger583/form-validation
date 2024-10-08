0994a6b2a6a8cb43e1953ce3e6dcb8a206223f73
cozy <cozychen@liontravel.com>
2024-10-08 11:57:55 +0800

    feat: add button ui

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { cN } from '../../libs/tw-marge'
+
+interface ButtonProps extends React.ComponentProps<'button'> {
+  variant?: 'validation' | 'remove' | 'add' | 'disabled' | 'default';
+}
+
+export function Button({ children, variant = 'default', className, ...props }: ButtonProps) {
+    const baseStyle = 'text-sm rounded cursor-pointer p-2'
+    const variantStyle = {
+        default:'text-sm rounded font-bold text-black-500 cursor-pointer',
+        validation:'text-white bg-orange-500',
+        remove:'text-orange-500 font-bold',
+        add:'text-green-500 py-2 px-0',
+        disabled:'text-slate-500 py-2 px-0 cursor-not-allowed',
+    }
+  return (
+    <button {...props} className={cN(baseStyle ,variantStyle[variant], className)}>
+      {children}
+    </button>
+  )
+}
